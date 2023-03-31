914d1f7b3b6cf1e6015ea3c012d2640ff47491ec
stranger <subzero2353@gmail.com>
2023-03-31 18:36:13 +0300

    added needed end points for now

--- src/main/java/com/hope/sps/zone/ZoneController.java
+++ src/main/java/com/hope/sps/zone/ZoneController.java
@@ -1,7 +1,64 @@
 package com.hope.sps.zone;
 
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("api/v1/zone")
+@PreAuthorize("hasAuthority('ADMIN')")
 public class ZoneController {
-    // addZone all zone model
-    // make new spaces generated
-    //
+
+    private final ZoneService zoneService;
+
+    @GetMapping
+    public ResponseEntity<List<ZoneDTO>> getAll() {
+
+        List<ZoneDTO> allZones = zoneService.getAll();
+
+        return ResponseEntity.ok(allZones);
+    }
+
+    @PostMapping
+    public ResponseEntity<Long> registerZone(
+            @RequestBody
+            @Valid
+            ZoneRegistrationRequest request
+    ) {
+
+        Long zoneId = zoneService.registerZone(request);
+
+        return ResponseEntity.ok(zoneId);
+    }
+
+    @PutMapping("{zoneId}")
+    public ResponseEntity<Long> updateZone(
+            @RequestBody
+            @Valid
+            ZoneUpdateRequest request,
+            @PathVariable
+            Long zoneId
+    ) {
+
+        zoneService.updateZone(zoneId, request);
+
+        return ResponseEntity.ok(zoneId);
+    }
+
+    @DeleteMapping("{zoneId}")
+    public ResponseEntity<Void> removeZone(
+            @PathVariable("zoneId")
+            Long zoneId
+    ) {
+
+        zoneService.removeZone(zoneId);
+
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
 }
