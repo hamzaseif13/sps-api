7a9526ee4f8e2c7ac14479be30f21b3379f47c00
hamza <hamzaseif1333@gmail.com>
2023-04-02 18:40:25 +0300

    added admin DTO

---
+++ src/main/java/com/hope/sps/admin/AdminDto.java
@@ -0,0 +1,7 @@
+package com.hope.sps.admin;
+
+public record AdminDto(  Long id,
+                         String firstName,
+                         String lastName,
+                         String email) {
+}
