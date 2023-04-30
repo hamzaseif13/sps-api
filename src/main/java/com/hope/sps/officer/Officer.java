0095abb2f82d6f2c53ec984718d87ca406477931
stranger <subzero2353@gmail.com>
2023-04-30 18:40:43 +0300

    making phoneNumber string

--- src/main/java/com/hope/sps/officer/Officer.java
+++ src/main/java/com/hope/sps/officer/Officer.java
@@ -28,11 +28,17 @@ public class Officer extends BaseEntity {
     @JoinColumn(name = "schedule_id")
     private Schedule schedule;
 
-    private Long phone;
+    private String phone;
+
     @OneToMany(fetch = FetchType.LAZY)
     @JoinColumn(name = "officer_id")
     private Set<Zone> zones;
 
-
-
+    public Officer(Long id, UserDetailsImpl userDetails, Schedule schedule, String phone, Set<Zone> zones) {
+        super(id);
+        this.userDetails = userDetails;
+        this.schedule = schedule;
+        this.phone = phone;
+        this.zones = zones;
+    }
 }
