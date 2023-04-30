d067fc39b2f16f5f3c2cb35ac9514d2b1fe30721
stranger <subzero2353@gmail.com>
2023-04-30 18:39:47 +0300

    modified to use modelMapper and registerCustomer returns authentication response

--- src/main/java/com/hope/sps/customer/CustomerService.java
+++ src/main/java/com/hope/sps/customer/CustomerService.java
@@ -1,24 +1,37 @@
 package com.hope.sps.customer;
 
+import com.hope.sps.UserDetails.Role;
 import com.hope.sps.UserDetails.UserDetailsImpl;
-import com.hope.sps.dto.RegisterRequestMapper;
+import com.hope.sps.auth.AuthenticationResponse;
+import com.hope.sps.customer.payment.Wallet;
+import com.hope.sps.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
-
-    private final RegisterRequestMapper registerRequestMapper;
+    private final ModelMapper mapper;
 
     private final CustomerRepository customerRepository;
 
-    public Long registerCustomer(CustomerRegisterRequest request) {
-        UserDetailsImpl customerDetails = registerRequestMapper.apply(request);
-        Wallet customerWallet = new Wallet(0.0);
-        Customer customer = new Customer(customerDetails, customerWallet);
+    private final JwtUtils jwtUtils;
 
-        return customerRepository.save(customer).getId();
+    private final PasswordEncoder passwordEncoder;
+
+    public AuthenticationResponse registerCustomer(CustomerRegisterRequest request) {
+        final UserDetailsImpl customerDetails = mapper.map(request, UserDetailsImpl.class);
+        final Wallet customerWallet = new Wallet(10.0);
+        final Customer customer = new Customer(customerDetails, customerWallet, request.getPhoneNumber());
+        customerDetails.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        customerRepository.save(customer);
+
+        final String jwtToken = jwtUtils.generateToken(customerDetails);
+
+        return new AuthenticationResponse(request.getEmail(), jwtToken, Role.CUSTOMER);
     }
 }
