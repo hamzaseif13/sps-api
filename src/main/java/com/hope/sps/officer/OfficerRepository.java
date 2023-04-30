09a652f2b617dc23b54d95f50e2964fd1e9f12a4
stranger <subzero2353@gmail.com>
2023-04-30 18:41:57 +0300

    removed unused method

--- src/main/java/com/hope/sps/officer/OfficerRepository.java
+++ src/main/java/com/hope/sps/officer/OfficerRepository.java
@@ -1,11 +1,6 @@
 package com.hope.sps.officer;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 public interface OfficerRepository extends JpaRepository<Officer, Long> {
-
-    @Query("SELECT o.id FROM Officer o WHERE o.userDetails.id =:uDetailsId")
-    Long getOfficerIdByUserDetailsId(@Param("uDetailsId") Long userDetailsId);
 }
