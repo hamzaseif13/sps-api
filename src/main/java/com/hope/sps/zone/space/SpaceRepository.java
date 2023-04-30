9dbea8f4365e8f42e0fe3aec36ddb53ff7e4f448
stranger <subzero2353@gmail.com>
2023-04-30 18:44:08 +0300

    added new method existsByIdAndStateIs

--- src/main/java/com/hope/sps/zone/space/SpaceRepository.java
+++ src/main/java/com/hope/sps/zone/space/SpaceRepository.java
@@ -3,13 +3,16 @@ package com.hope.sps.zone.space;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 public interface SpaceRepository extends JpaRepository<Space, Long> {
 
+    boolean existsByIdAndStateIs(Long id, Space.State state);
 
-    @Query(value = "delete from space where zone_id = ?1 ",nativeQuery = true)
+
+    @Query(value = "delete from space where zone_id =:id ", nativeQuery = true)
     @Transactional
     @Modifying
-    void removeAllByZoneId(Long zoneId);
+    void removeAllByZoneId(@Param("id") Long zoneId);
 }
