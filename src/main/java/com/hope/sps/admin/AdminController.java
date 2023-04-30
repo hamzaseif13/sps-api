bcad1196518709dde3593421fadc8f08ea0fd876
stranger <subzero2353@gmail.com>
2023-04-30 18:30:32 +0300

    get all admins added

--- src/main/java/com/hope/sps/admin/AdminController.java
+++ src/main/java/com/hope/sps/admin/AdminController.java
@@ -3,8 +3,12 @@ package com.hope.sps.admin;
 
 import com.hope.sps.dto.RegisterRequest;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -12,30 +16,44 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/admin")
 @RequiredArgsConstructor
+@PreAuthorize("hasRole('ADMIN')")
 public class AdminController {
 
     private final AdminService adminService;
 
-    //register admin Done
+    @GetMapping
+    public ResponseEntity<List<AdminDTO>> getAll() {
+
+        final List<AdminDTO> adminDTOS = adminService.getAllAdmins();
+
+        if (adminDTOS.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+        return ResponseEntity.ok(adminDTOS);
+    }
+
     @PostMapping
     public ResponseEntity<Long> register(
             @RequestBody
             @Valid
-            RegisterRequest request) {
+            RegisterRequest request
+    ) {
 
-        Long adminId = adminService.registerAdmin(request);
+        final Long adminId = adminService.registerAdmin(request);
 
         return ResponseEntity.ok(adminId);
     }
-    @GetMapping
-    public ResponseEntity<List<AdminDto>> getAllAdmins(){
-        return ResponseEntity.ok(adminService.getAllAdmins());
-    }
 
     @DeleteMapping("{id}")
-    public ResponseEntity<String>deleteAdminById(@PathVariable Long id){
+    public ResponseEntity<Void> deleteById(
+            @PathVariable
+            @Validated
+            @Positive
+            Long id
+    ) {
+
         adminService.deleteAdminById(id);
-        return ResponseEntity.ok("delete Successfully");
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 
 }
