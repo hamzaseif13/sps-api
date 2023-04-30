0049b4805f79f25f1f951e440ece3a0327c6da41
stranger <subzero2353@gmail.com>
2023-04-30 18:37:39 +0300

    make phoneNumber string

--- src/main/java/com/hope/sps/customer/Customer.java
+++ src/main/java/com/hope/sps/customer/Customer.java
@@ -1,7 +1,10 @@
 package com.hope.sps.customer;
 
+import com.hope.sps.UserDetails.Role;
 import com.hope.sps.UserDetails.UserDetailsImpl;
 import com.hope.sps.booking.BookingSession;
+import com.hope.sps.customer.car.Car;
+import com.hope.sps.customer.payment.Wallet;
 import com.hope.sps.model.BaseEntity;
 import jakarta.persistence.*;
 import lombok.*;
@@ -14,29 +17,35 @@ import java.util.Set;
 @AllArgsConstructor
 @NoArgsConstructor
 @ToString
+@Setter
+@Getter
 @EqualsAndHashCode(callSuper = true)
 public class Customer extends BaseEntity {
-
-    @OneToMany(fetch = FetchType.EAGER)
-    @JoinColumn(name = "customer_id")
-    private Set<BookingSession> bookingHistory;
 
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private UserDetailsImpl userDetails;
 
-    private Long phone;
+    @Column(name = "phone_number",length = 20)
+    private String phoneNumber;
+
     @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
     private Wallet wallet;
 
     @OneToOne(cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     private BookingSession activeBookingSession;
 
+    @OneToMany(fetch = FetchType.EAGER)
+    @JoinColumn(name = "customer_id")
+    private Set<BookingSession> bookingHistory;
+
     @OneToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
     @JoinColumn(name = "customer_id")
     private Set<Car> cars;
 
-    public Customer(UserDetailsImpl userDetails, Wallet wallet) {
+    public Customer(UserDetailsImpl userDetails, Wallet wallet, String phoneNumber) {
         this.userDetails = userDetails;
         this.wallet = wallet;
+        this.phoneNumber = phoneNumber;
+        userDetails.setRole(Role.CUSTOMER);
     }
 }
