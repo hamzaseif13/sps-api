03db3f65cbb1ac0bd9bab32fdd0527efb4ec1eb8
stranger <subzero2353@gmail.com>
2023-05-01 23:53:04 +0300

    added getCurrentBookingSession, extendCurrentSession and startNewBookingSession methods

--- src/main/java/com/hope/sps/booking/BookingService.java
+++ src/main/java/com/hope/sps/booking/BookingService.java
@@ -1,16 +1,21 @@
 package com.hope.sps.booking;
 
-import com.hope.sps.UserDetails.UserDetailsImpl;
+import com.hope.sps.UserInformation.UserInformation;
 import com.hope.sps.customer.Customer;
 import com.hope.sps.customer.CustomerRepository;
 import com.hope.sps.customer.car.Car;
+import com.hope.sps.customer.payment.wallet.Wallet;
+import com.hope.sps.customer.payment.wallet.WalletRepository;
 import com.hope.sps.exception.InsufficientWalletBalanceException;
+import com.hope.sps.exception.InvalidResourceProvidedException;
+import com.hope.sps.exception.ResourceNotFoundException;
 import com.hope.sps.zone.ZoneRepository;
 import com.hope.sps.zone.space.Space;
 import com.hope.sps.zone.space.SpaceRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 import java.util.Set;
@@ -27,67 +32,104 @@ public class BookingService {
 
     private final SpaceRepository spaceRepository;
 
+    private final WalletRepository walletRepository;
 
-    public Optional<BookingDTO> getCurrentBookingSession(final Authentication authentication) {
-        final Customer loggedInCustomer = getLoggedInCustomer(authentication);
+    private final ModelMapper mapper;
+
+
+    @Transactional(readOnly = true)
+    public Optional<BookingDTO> getCurrentBookingSession(final UserInformation userInformation) {
+        final Customer loggedInCustomer = customerRepository
+                .findByUserInformationEmail(userInformation.getEmail())
+                .orElseThrow();
+
         final BookingDTO bookingDTO;
+        final var currentBookingSession = loggedInCustomer.getActiveBookingSession();
+        final Set<BookingSession> bookingHistory = loggedInCustomer.getBookingHistory();
 
-        if (loggedInCustomer.getActiveBookingSession() != null) {
-            bookingDTO = new BookingDTO(Set.of(loggedInCustomer.getActiveBookingSession()), BookingState.CURRENT.name());
+        if (currentBookingSession != null) {
+            bookingDTO = mapper.map(currentBookingSession, BookingDTO.class);
+            bookingDTO.setObjectState(BookingState.CURRENT.name());
+        } else if (bookingHistory != null && !bookingHistory.isEmpty()) {
+            bookingDTO = mapper.map(bookingHistory.stream().findFirst().get(), BookingDTO.class);
+            bookingDTO.setObjectState(BookingState.PREVIOUS.name());
         } else {
-            if (loggedInCustomer.getBookingHistory() == null || loggedInCustomer.getBookingHistory().isEmpty()) {
-                return Optional.empty();
-            } else {//modify to last boooking session todo
-                bookingDTO = new BookingDTO(loggedInCustomer.getBookingHistory(), BookingState.PREVIOUS.name());
-            }
-        }//duration in ms
+            return Optional.empty();
+        }
 
         return Optional.of(bookingDTO);
     }
 
-    public void extendCurrentSession(final Long currentSessionId, final Long durationInMS) {
-        if (bookingSessionRepository.existsById(currentSessionId)) {
-            final Long durationInMinutes = durationInMS / 60_000;
-            bookingSessionRepository.extendDuration(currentSessionId, durationInMinutes);
+    @Transactional
+    public void extendCurrentSession(
+            final Long currentSessionId,
+            final ExtendCurrentSessionRequest request,
+            final UserInformation userInformation
+    ) {
+        if (!bookingSessionRepository.existsById(currentSessionId)) {
+            throw new ResourceNotFoundException("there is no active booking session");
         }
+
+        final Customer loggedInCustomer = customerRepository
+                .findByUserInformationEmail(userInformation.getEmail())
+                .orElseThrow();
+
+        final var customerWallet = loggedInCustomer.getWallet();
+        final long durationInMinutes = request.getDurationInMs() / 60_000;
+        final double totalFeeToStrip = getTotalFeeToStrip(request.getZoneId(), customerWallet, durationInMinutes);
+
+        walletRepository.updateBalance(customerWallet.getId(), -totalFeeToStrip);
+        bookingSessionRepository.extendDuration(currentSessionId, request.getDurationInMs());
     }
 
-    public Long startNewBookingSession(final Authentication authentication, final BookingSessionRequest request) {
-        if(spaceRepository.existsByIdAndStateIs(request.getSpaceId(),Space.State.TAKEN)){
-
-
+    @Transactional
+    public Long startNewBookingSession(final UserInformation userInformation, final BookingSessionRequest request) {
+        if (spaceRepository.existsByIdAndStateIs(request.getSpaceId(), Space.State.TAKEN)) {
+            throw new InvalidResourceProvidedException("space already taken");
         }
 
-        final Customer loggedInCustomer = getLoggedInCustomer(authentication);
-        final long durationInMinutes = request.getDurationInMs() / 60_000;
+        final Customer loggedInCustomer = customerRepository
+                .findByUserInformationEmail(userInformation.getEmail())
+                .orElseThrow();
+
+        if (loggedInCustomer.getActiveBookingSession() != null) {
+            throw new InvalidResourceProvidedException("customer already have active booking session");
+        }
 
         var newBookingSession = BookingSession.builder()
                 .car(new Car(request.getCarId()))
-                .space(new Space(request.getSpaceId(),Space.State.TAKEN))
-                .duration(durationInMinutes)
+                .space(new Space(request.getSpaceId()))
+                .duration(request.getDurationInMs())
                 .extended(false)
                 .state(BookingSession.State.ACTIVE)
+                .customer(loggedInCustomer)
                 .build();
 
-        final double zoneFee = zoneRepository.getFeeById(request.getZoneId());
+        final var customerWallet = loggedInCustomer.getWallet();
+        final long durationInMinutes = request.getDurationInMs() / 60_000;
+        final double totalFeeToStrip = getTotalFeeToStrip(request.getZoneId(), customerWallet, durationInMinutes);
+
+        spaceRepository.updateSpaceState(request.getSpaceId(), Space.State.TAKEN);
+        walletRepository.updateBalance(customerWallet.getId(), -totalFeeToStrip);
+
+        final var createdBookingSession = bookingSessionRepository.save(newBookingSession);
+        loggedInCustomer.setActiveBookingSession(createdBookingSession);
+
+        return createdBookingSession.getId();
+    }
+
+    private double getTotalFeeToStrip(
+            final Long zoneId,
+            final Wallet customerWallet,
+            final Long durationInMinutes
+    ) {
+        final double zoneFee = zoneRepository.getFeeById(zoneId);
         final double total = ((double) durationInMinutes / 60) * zoneFee;
-        final double totalAfterStriping = loggedInCustomer.getWallet().getBalance() - total;
+        final double totalAfterStriping = customerWallet.getBalance() - total;
 
         if (totalAfterStriping < 0)
             throw new InsufficientWalletBalanceException("Insufficient balance please charge your wallet");
-
-        loggedInCustomer.getWallet().setBalance(totalAfterStriping);
-        loggedInCustomer.setActiveBookingSession(newBookingSession);
-
-        return bookingSessionRepository.save(newBookingSession).getId();
-    }
-
-
-    private Customer getLoggedInCustomer(Authentication authentication) {
-        final UserDetailsImpl customerUserDetails = (UserDetailsImpl) authentication.getPrincipal();
-        return customerRepository
-                .findByUserDetailsEmail(customerUserDetails.getEmail())
-                .orElseThrow();
+        return total;
     }
 
     enum BookingState {
