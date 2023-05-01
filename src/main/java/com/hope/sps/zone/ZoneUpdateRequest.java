cae7375fbd2c9185448f4492d7c70b0ed4972a45
stranger <subzero2353@gmail.com>
2023-05-02 00:13:03 +0300

    added validations

--- src/main/java/com/hope/sps/zone/ZoneUpdateRequest.java
+++ src/main/java/com/hope/sps/zone/ZoneUpdateRequest.java
@@ -1,12 +1,29 @@
 package com.hope.sps.zone;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.sql.Time;
 
-public record ZoneUpdateRequest(
-        Double fee,
-        String title,
-        Integer numberOfSpaces,
-        Time startsAt,
-        Time endsAt
-) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ZoneUpdateRequest {
+
+    @NotEmpty(message = "title is required")
+    private String title;
+
+    @NotEmpty(message = "fee is required")
+    private Double fee;
+
+    @NotEmpty(message = "numberOfSpaces is required")
+    private Integer numberOfSpaces;
+
+    @NotEmpty(message = "startsAt is required")
+    private Time startsAt;
+
+    @NotEmpty(message = "endsAt is required")
+    private Time endsAt;
 }
