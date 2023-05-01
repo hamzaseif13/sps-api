bfea8318735a9279d7fce33bbd540ffacb2894c8
stranger <subzero2353@gmail.com>
2023-05-02 00:01:26 +0300

    renamed from InvalidResourceException to InvalidResourceProvidedException

--- src/main/java/com/hope/sps/exception/InvalidResourceProvidedException.java
+++ src/main/java/com/hope/sps/exception/InvalidResourceProvidedException.java
@@ -4,9 +4,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value = HttpStatus.BAD_REQUEST)
-public class InvalidResourceException extends RuntimeException {
+public class InvalidResourceProvidedException extends RuntimeException {
 
-    public InvalidResourceException(String message) {
+    public InvalidResourceProvidedException(String message) {
         super(message);
     }
 }
