f2235fb5c077b8078620733d2482cb5112367719
stranger <subzero2353@gmail.com>
2023-05-01 23:52:19 +0300

    modified to have more info

--- src/main/java/com/hope/sps/booking/BookingDTO.java
+++ src/main/java/com/hope/sps/booking/BookingDTO.java
@@ -1,15 +1,33 @@
 package com.hope.sps.booking;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.hope.sps.customer.car.Car;
+import com.hope.sps.zone.space.Space;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.Set;
+import java.time.LocalDateTime;
 
-@AllArgsConstructor
 @Data
-public final class BookingDTO {
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({"id", "createdAt", "duration", "space", "car", "state", "extended", "objectState"})
+public class BookingDTO {
+    //i can do better if i have zone tag
+    private Long id;
 
-    private Set<BookingSession> bookingSession;
+    private LocalDateTime createdAt;
 
-    private String state;
+    private Long duration;
+
+    private Space space;
+
+    private Car car;
+
+    private BookingSession.State state;
+
+    private Boolean extended;
+
+    private String objectState;
 }
