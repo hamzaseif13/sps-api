61752d4e01bb27f06924668552d198d4445d4279
stranger <subzero2353@gmail.com>
2023-05-02 00:04:00 +0300

    converted to class for modelMapper to work and added validations

--- src/main/java/com/hope/sps/auth/LoginRequest.java
+++ src/main/java/com/hope/sps/auth/LoginRequest.java
@@ -1,5 +1,21 @@
 package com.hope.sps.auth;
 
-public record LoginRequest(String email, String password) {
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public final class LoginRequest {
+
+    @Email(message = "invalid email")
+    @NotEmpty(message = "password is mandatory")
+    private String email;
+
+    @Size(min = 8, max = 64, message = "invalid password size")
+    @NotEmpty(message = "password is mandatory")
+    private String password;
 
 }
