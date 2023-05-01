c3a733d69b2431afed6470a703279b0d058b5079
stranger <subzero2353@gmail.com>
2023-05-01 23:49:25 +0300

    added first and last name to the response

--- src/main/java/com/hope/sps/auth/AuthenticationResponse.java
+++ src/main/java/com/hope/sps/auth/AuthenticationResponse.java
@@ -1,11 +1,14 @@
 package com.hope.sps.auth;
 
-import com.hope.sps.UserDetails.Role;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import com.hope.sps.UserInformation.Role;
 
+@JsonPropertyOrder({"firstName", "lastName", "email", "jwtToken", "role"})
 public record AuthenticationResponse(
         String email,
         String jwtToken,
-        Role role
-        //first name and lastName
+        Role role,
+        String firstName,
+        String lastName
 ) {
 }
