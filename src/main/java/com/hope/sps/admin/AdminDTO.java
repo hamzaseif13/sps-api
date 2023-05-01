576680bb573e2dfddad91e00552365749775a4c8
stranger <subzero2353@gmail.com>
2023-05-01 19:26:51 +0300

    converted from record to class, because the model mapper library follows bean conventions

--- src/main/java/com/hope/sps/admin/AdminDTO.java
+++ src/main/java/com/hope/sps/admin/AdminDTO.java
@@ -1,9 +1,22 @@
 package com.hope.sps.admin;
 
-public record AdminDTO(
-        Long id,
-        String firstName,
-        String lastName,
-        String email
-) {
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({"id", "firstName", "lastName", "email"})
+public class AdminDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
 }
