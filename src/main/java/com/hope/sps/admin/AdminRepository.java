386a53a44b61f879f7ea138be0135065e3ba741b
stranger <subzero2353@gmail.com>
2023-05-01 19:27:15 +0300

    added boolean existsByUserInformationEmail(String email) method

--- src/main/java/com/hope/sps/admin/AdminRepository.java
+++ src/main/java/com/hope/sps/admin/AdminRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminRepository extends JpaRepository<Admin, Long> {
 
+    boolean existsByUserInformationEmail(String email);
 }
