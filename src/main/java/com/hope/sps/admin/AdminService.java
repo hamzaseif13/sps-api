c21c0ab02b76718de8f20b2668614ebd731fe608
stranger <subzero2353@gmail.com>
2023-05-01 23:45:29 +0300

    fixed password wasn't encrypted when register new admin, refactored to use ModelMapper and added check condition inside deleteAdminById method

--- src/main/java/com/hope/sps/admin/AdminService.java
+++ src/main/java/com/hope/sps/admin/AdminService.java
@@ -1,10 +1,19 @@
 package com.hope.sps.admin;
 
-import com.hope.sps.dto.RegisterRequest;
+import com.hope.sps.UserInformation.Role;
+import com.hope.sps.UserInformation.UserInformation;
+import com.hope.sps.common.RegisterRequest;
+import com.hope.sps.exception.DuplicateResourceException;
+import com.hope.sps.exception.InvalidResourceProvidedException;
+import com.hope.sps.exception.ResourceNotFoundException;
+import com.hope.sps.util.Validator;
 import lombok.RequiredArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -14,29 +23,41 @@ public class AdminService {
 
     private final AdminRepository adminRepository;
 
-    private final EmployeeRegisterRequestMapper employeeRegisterRequestMapper;
-
-    private final AdminDTOMapper adminDTOMapper;
-
     private final PasswordEncoder passwordEncoder;
 
+    private final ModelMapper mapper;
+
+    @Transactional(readOnly = true)
     public List<AdminDTO> getAllAdmins() {
-        return adminRepository.findAll()
-                .stream()
-                .map(adminDTOMapper)
-                .toList();
+
+        final List<AdminDTO> adminDTOS = new ArrayList<>();
+
+        adminRepository.findAll().forEach(admin -> {
+            adminDTOS.add(mapper.map(admin, AdminDTO.class));
+        });
+
+        return adminDTOS;
     }
 
     public Long registerAdmin(final RegisterRequest request) {
+        if (adminRepository.existsByUserInformationEmail(request.getEmail()))
+            throw new DuplicateResourceException("email already exists");
 
-        var adminDetails = employeeRegisterRequestMapper.apply(request);
+        final var adminDetails = mapper.map(request, UserInformation.class);//employeeRegisterRequestMapper.apply(request);
+        adminDetails.setRole(Role.ADMIN);
+
+        if (!request.getPassword().matches(Validator.passwordValidationRegex))
+            throw new InvalidResourceProvidedException("invalid password");
+
         adminDetails.setPassword(passwordEncoder.encode(request.getPassword()));
-
-        System.err.println(adminDetails);
         return adminRepository.save(new Admin(adminDetails)).getId();
     }
 
-    public void deleteAdminById(final Long id) {
-        adminRepository.deleteById(id);
+    public void deleteAdminById(final Long adminId) {
+        if (!adminRepository.existsById(adminId)) {
+            throw new ResourceNotFoundException("could not delete admin with id: {%s}, no admin found".formatted(adminId));
+        }
+
+        adminRepository.deleteById(adminId);
     }
 }
