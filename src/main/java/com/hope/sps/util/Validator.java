9a8c255152d43a5efe1d9029fd9d1e4a2d976a13
stranger <subzero2353@gmail.com>
2023-05-02 00:13:39 +0300

    Validator

---
+++ src/main/java/com/hope/sps/util/Validator.java
@@ -0,0 +1,7 @@
+package com.hope.sps.util;
+
+public class Validator {
+
+    public static final String passwordValidationRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+
+}
