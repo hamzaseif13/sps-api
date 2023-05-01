7c10301ba6325150e2a4de592945bd79cf285a22
stranger <subzero2353@gmail.com>
2023-05-02 00:00:19 +0300

    added getAllCustomers, findById and enhanced the registerCustomer method

--- src/main/java/com/hope/sps/customer/CustomerService.java
+++ src/main/java/com/hope/sps/customer/CustomerService.java
@@ -1,37 +1,78 @@
 package com.hope.sps.customer;
 
-import com.hope.sps.UserDetails.Role;
-import com.hope.sps.UserDetails.UserDetailsImpl;
+import com.hope.sps.UserInformation.Role;
+import com.hope.sps.UserInformation.UserInformation;
 import com.hope.sps.auth.AuthenticationResponse;
-import com.hope.sps.customer.payment.Wallet;
+import com.hope.sps.customer.payment.wallet.Wallet;
+import com.hope.sps.exception.DuplicateResourceException;
+import com.hope.sps.exception.InvalidResourceProvidedException;
+import com.hope.sps.exception.ResourceNotFoundException;
 import com.hope.sps.jwt.JwtUtils;
+import com.hope.sps.util.Validator;
 import lombok.RequiredArgsConstructor;
 import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class CustomerService {
 
-    private final ModelMapper mapper;
-
     private final CustomerRepository customerRepository;
-
-    private final JwtUtils jwtUtils;
 
     private final PasswordEncoder passwordEncoder;
 
-    public AuthenticationResponse registerCustomer(CustomerRegisterRequest request) {
-        final UserDetailsImpl customerDetails = mapper.map(request, UserDetailsImpl.class);
-        final Wallet customerWallet = new Wallet(10.0);
-        final Customer customer = new Customer(customerDetails, customerWallet, request.getPhoneNumber());
-        customerDetails.setPassword(passwordEncoder.encode(request.getPassword()));
+    private final JwtUtils jwtUtils;
+
+    private final ModelMapper mapper;
+
+    @Transactional(readOnly = true)
+    public List<CustomerDTO> getAllCustomers() {
+        var customerDTOs = new ArrayList<CustomerDTO>();
+
+        customerRepository.findAll().forEach(customer ->
+                customerDTOs.add(
+                        mapper.map(customer, CustomerDTO.class)
+                ));
+
+        return customerDTOs;
+    }
+
+    public AuthenticationResponse registerCustomer(final CustomerRegisterRequest request) {
+        if (customerRepository.existsByUserInformationEmail(request.getEmail()))
+            throw new DuplicateResourceException("email already exists");
+
+        if (!request.getPassword().matches(Validator.passwordValidationRegex))
+            throw new InvalidResourceProvidedException("invalid password");
+
+        final var customerInformation = mapper.map(request, UserInformation.class);
+        customerInformation.setRole(Role.CUSTOMER);
+        customerInformation.setPassword(passwordEncoder.encode(request.getPassword()));
+
+        final var customerWallet = new Wallet(10.0);
+
+        final var customer = new Customer(customerInformation, customerWallet, request.getPhoneNumber());
 
         customerRepository.save(customer);
 
-        final String jwtToken = jwtUtils.generateToken(customerDetails);
+        final String jwtToken = jwtUtils.generateToken(customerInformation);
+        return new AuthenticationResponse(
+                request.getEmail(),
+                jwtToken,
+                Role.CUSTOMER,
+                customerInformation.getFirstName(),
+                customerInformation.getLastName()
+        );
+    }
 
-        return new AuthenticationResponse(request.getEmail(), jwtToken, Role.CUSTOMER);
+    public CustomerDTO findById(Long customerId) {
+        return mapper.map(customerRepository.findById(customerId)
+                .orElseThrow(() -> new ResourceNotFoundException("customer doesn't exists")
+                ), CustomerDTO.class
+        );
     }
 }
