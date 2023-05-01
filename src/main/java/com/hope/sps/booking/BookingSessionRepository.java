9f01e9dc64eb06d62cfb1402ad6a6b999a8e108c
stranger <subzero2353@gmail.com>
2023-05-01 23:54:20 +0300

    added @Modifying annotation

--- src/main/java/com/hope/sps/booking/BookingSessionRepository.java
+++ src/main/java/com/hope/sps/booking/BookingSessionRepository.java
@@ -1,11 +1,13 @@
 package com.hope.sps.booking;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 public interface BookingSessionRepository extends JpaRepository<BookingSession, Long> {
 
-    @Query("UPDATE BookingSession b SET b.extended=true, b.duration = b.duration+:durationInMin WHERE b.id=:BS_ID")
-    void extendDuration(@Param("BS_ID") Long bookingSessionId,@Param("durationInMin") Long durationInMinutes);
+    @Modifying
+    @Query("UPDATE BookingSession b SET b.extended=true, b.duration = b.duration+:durationInMs WHERE b.id=:BS_ID")
+    void extendDuration(@Param("BS_ID") Long bookingSessionId, @Param("durationInMs") Long durationInMS);
 }
