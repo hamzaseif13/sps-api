97b123080888347d253436e9a90379b8e3dffa2c
stranger <subzero2353@gmail.com>
2023-05-01 23:58:30 +0300

    add getOne and getAll methods

--- src/main/java/com/hope/sps/customer/CustomerController.java
+++ src/main/java/com/hope/sps/customer/CustomerController.java
@@ -3,8 +3,12 @@ package com.hope.sps.customer;
 
 import com.hope.sps.auth.AuthenticationResponse;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -16,32 +20,35 @@ public class CustomerController {
 
     private final CustomerService customerService;
 
+    @PreAuthorize("hasAuthority('ADMIN')")
     @GetMapping
     public ResponseEntity<List<CustomerDTO>> getAll() {
+        var customerDTOs = customerService.getAllCustomers();
 
-        return null;
+        if (customerDTOs.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return ResponseEntity.ok(customerDTOs);
     }
+
+    @PreAuthorize("hasAuthority('ADMIN')")
+    @GetMapping("{customerId}")
+    public ResponseEntity<CustomerDTO> getOne(
+            @PathVariable("customerId")
+            @Validated @Positive
+            Long customerId
+    ) {
+        var customerDTOs = customerService.findById(customerId);
+        return ResponseEntity.ok(customerDTOs);
+    }
+
 
     @PostMapping
     public ResponseEntity<AuthenticationResponse> registerCustomer(
-            @Valid
             @RequestBody
+            @Valid
             CustomerRegisterRequest request) {
 
         final AuthenticationResponse authResp = customerService.registerCustomer(request);
-
         return ResponseEntity.ok(authResp);
-    }
-
-    @PutMapping
-    public ResponseEntity<Long> updateCustomer() {
-
-        return null;
-    }
-
-    @DeleteMapping
-    public ResponseEntity<Void> deleteCustomer() {
-
-        return null;
     }
 }
