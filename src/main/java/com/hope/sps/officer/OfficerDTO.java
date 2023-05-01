aa817ff87d8075d570dd40572701c1a60f2a40ce
stranger <subzero2353@gmail.com>
2023-05-02 00:05:34 +0300

    converted to class so ModelMapper works

--- src/main/java/com/hope/sps/officer/OfficerDTO.java
+++ src/main/java/com/hope/sps/officer/OfficerDTO.java
@@ -1,18 +1,32 @@
 package com.hope.sps.officer;
 
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import com.hope.sps.officer.schedule.Schedule;
 import com.hope.sps.zone.Zone;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Set;
 
-public record OfficerDTO(
-        Long id,
-        String firstName,
-        String lastName,
-        String email,
-        Schedule schedule,
-        Set<Zone> zones,
-        String phone
-) {
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@JsonPropertyOrder({"id", "firstName", "lastName", "email", "phoneNumber", "schedule", "zones"})
+public class OfficerDTO {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String phoneNumber;
+
+    private Schedule schedule;
+
+    private Set<Zone> zones;
 
 }
