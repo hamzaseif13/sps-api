1d9880614964084d3ea30ff6a2fc7b4eda55a5f5
ABOOOD38 <subzero2353@gmail.com>
2023-06-06 21:20:54 +0300

    add validation on space occupation

--- src/main/java/com/hope/sps/zone/space/SpaceService.java
+++ src/main/java/com/hope/sps/zone/space/SpaceService.java
@@ -3,8 +3,11 @@ package com.hope.sps.zone.space;
 import com.hope.sps.booking.BookingSession;
 import com.hope.sps.customer.Customer;
 import com.hope.sps.customer.CustomerRepository;
+import com.hope.sps.exception.InvalidResourceProvidedException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -28,14 +31,18 @@ public class SpaceService {
     }
 
     public OccupiedSpaceDTO getOccupiedSpaceInformation(final Long spaceId) {
-        final Customer occupiedSpaceCustomer = customerRepository
-                .findByActiveBookingSessionSpaceIdAndActiveBookingSessionState(spaceId, BookingSession.State.ACTIVE).orElseThrow();
+        final Optional<Customer> occupiedSpaceCustomer = customerRepository
+                .findByActiveBookingSessionSpaceIdAndActiveBookingSessionState(spaceId, BookingSession.State.ACTIVE);
 
-        final String customerFirstName = occupiedSpaceCustomer.getUserInformation().getFirstName();
-        final String customerLastName = occupiedSpaceCustomer.getUserInformation().getLastName();
+        if (occupiedSpaceCustomer.isEmpty())
+            throw new InvalidResourceProvidedException("this space is not occupied");
+
+        final Customer customer = occupiedSpaceCustomer.get();
+        final String customerFirstName = customer.getUserInformation().getFirstName();
+        final String customerLastName = customer.getUserInformation().getLastName();
         final String customerName = String.format("%s %s", customerFirstName, customerLastName);
 
-        final BookingSession customerActiveBookingSession = occupiedSpaceCustomer.getActiveBookingSession();
+        final BookingSession customerActiveBookingSession = customer.getActiveBookingSession();
 
         return new OccupiedSpaceDTO(
                 customerName,
