06967d125ced4b590a49bf0b749437a094b031ee
ABOOOD38 <subzero2353@gmail.com>
2023-06-06 15:45:57 +0300

    make it component

--- src/main/java/com/hope/sps/util/Validator.java
+++ src/main/java/com/hope/sps/util/Validator.java
@@ -1,16 +1,12 @@
 package com.hope.sps.util;
 
-import com.hope.sps.exception.InvalidResourceProvidedException;
+import org.springframework.stereotype.Component;
 
+@Component
 public class Validator {
 
-    private static final String passwordValidationRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-
-    /**
-     * @throws InvalidResourceProvidedException if the passwordBeforeHashing not satisfying the validationRegex
-     */
-    public static void validateUserPassword(final String passwordBeforeHashing) {
-        if (!passwordBeforeHashing.matches(Validator.passwordValidationRegex))
-            throw new InvalidResourceProvidedException("invalid password");
+    public boolean validateUserPassword(final String passwordBeforeHashing) {
+       final String passwordValidationRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
+        return passwordBeforeHashing.matches(passwordValidationRegex);
     }
 }
