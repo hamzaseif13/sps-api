c777c70f685a0da2704aadd030db2d45667793f5
ABOOOD38 <subzero2353@gmail.com>
2023-06-12 17:59:20 +0300

    fix conflict

--- src/main/java/com/hope/sps/util/Validator.java
+++ src/main/java/com/hope/sps/util/Validator.java
@@ -6,15 +6,10 @@ import org.springframework.stereotype.Component;
 public class Validator {
 
     public boolean validateUserPassword(final String passwordBeforeHashing) {
-<<<<<<< Updated upstream
         final String passwordValidationRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
-        return passwordBeforeHashing.matches(passwordValidationRegex) || true;
-=======
         if (passwordBeforeHashing == null || passwordBeforeHashing.isBlank() || passwordBeforeHashing.isEmpty())
             return false;
 
-        final String passwordValidationRegex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=])(?=\\S+$).{8,}$";
         return passwordBeforeHashing.matches(passwordValidationRegex);
->>>>>>> Stashed changes
     }
 }
