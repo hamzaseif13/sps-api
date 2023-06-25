7d12bff49844892d78d7c61e70cb7dda9a9bac91
Hamza Seif <66957751+hamzaseif13@users.noreply.github.com>
2023-06-25 19:42:00 +0300

    Update PingPongController.java

--- src/main/java/com/hope/sps/PingPongController.java
+++ src/main/java/com/hope/sps/PingPongController.java
@@ -22,7 +22,7 @@ public class PingPongController {
     }
     @GetMapping("/ping")
     public PingPong getPingPong() {
-        return new PingPong("Pong: %s".formatted(++COUNTER));
+        return new PingPong("PongPongi: %s".formatted(++COUNTER));
     }
 
     @GetMapping("/servertime")
